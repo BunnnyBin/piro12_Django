e2e465602c34efedd56c98abf39d8b06a72549d1
BunnnyBin <dbqls8428@gmail.com>
2020-01-18 02:14:42 +0900

    13장

--- shop/views.py
+++ shop/views.py
@@ -1,6 +1,20 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from shop.models import Item
+
 
 def archives_year(request, year):
     return HttpResponse(f'{year}년도에 대한 내용')
+
+def item_list(request):
+    qs = Item.objects.all()
+
+    q = request.GET.get('q','')  # 검색 단어
+    if q:
+        qs = qs.filter(name__icontains=q)
+
+    return render(request, 'shop/item_list.html', {
+        'item_list': qs,
+        'q': q  # 검색단어 남게하기
+    })
