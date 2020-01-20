55adcc1865cf63a19de85c0caaf1137b2897a410
BunnnyBin <dbqls8428@gmail.com>
2020-01-21 01:33:03 +0900

    26장

--- askcompany/urls.py
+++ askcompany/urls.py
@@ -1,4 +1,5 @@
 from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
@@ -7,6 +8,8 @@ urlpatterns = [
     path('shop/', include('shop.urls')),
     path('blog/', include('blog.urls')),
 ]
+# url 클릭하면 해당 이미지 보이게끔
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
     import debug_toolbar
