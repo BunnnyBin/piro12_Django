20a0b2752c89dc99efd3962ed717e20f1061832a
BunnnyBin <dbqls8428@gmail.com>
2020-01-21 12:19:38 +0900

    28장

--- shop/models.py
+++ shop/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 from askcompany.utils import uuid_upload_to
 
 class Item(models.Model):
@@ -12,3 +13,7 @@ class Item(models.Model):
 
     def __str__(self):
         return f'<{self.pk}> {self.name}'
+
+    @property
+    def get_absolute_url(self):
+        return reverse('shop:item_detail', args=[self.pk])
